ffd7a58a7101a19426d7d89bbe6fb6df22018b33
kato charles <ckcloudia@gmail.com>
2023-05-17 04:35:47 -0700

    refactor: use descrptive name and add comments to header file

--- shell.h
+++ shell.h
@@ -1,6 +1,10 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef SHELL_HEADER
+#define SHELL_HEADER
+
+/*Included header files*/
 #include <stdbool.h>
+
+/*prototypes*/
 int main(int argc, char *argv[], char **env);
 
 #endif
